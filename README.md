bcc3cdb97a4bbf6117d1d48fb30f7bae42cdb6e7
OsmanthusDev <osmanthusdev@outlook.com>
2022-05-30 17:30:10 +0800

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# Osmanthus Mono
+
+Rush monorepo
+
+
+## 安装 rush
+```shell
+npm install -g @microsoft/rush
+```
+
+## 安装依赖
+```shell
+rush update
+```
+
+## 添加依赖
+```shell
+cd apps/core-koa
+rush add --package http-errors
+```
---
+++ x.sh
@@ -0,0 +1,35 @@
+mkdir osmanthus-mono
+cd osmanthus-mono
+
+mkdir apps
+
+mkdir apps/core-koa
+mkdir apps/core-express
+mkdir apps/core-mongo
+mkdir apps/core-sqlite
+mkdir apps/core-mysql
+mkdir apps/core-redis
+mkdir apps/core-rabbit
+
+mkdir apps/core-component
+mkdir apps/core-composable
+
+mkdir apps/core-util
+mkdir apps/core-common
+
+mkdir apps/domain-system
+# mkdir apps/domain-material
+
+mkdir apps/app-rest
+
+# mkdir apps/app-mobile
+# mkdir apps/app-portal
+#
+#
+# npm init vue@latest
+
+
+
+cd apps/core-koa
+npm init -y
+cd ../../
